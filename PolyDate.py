ac0dac05948f47046a31b73409ce770cc7465d44
Aaron Stanek <amr.stanek@gmail.com>
2020-07-06 18:04:12 -0700

    now have comparison between dates

--- PolyDate.py
+++ PolyDate.py
@@ -1,6 +1,18 @@
 class PolyDate(object):
     def __init__(self,*args,**kwargs):
         raise Exception("Creating an instance of the base class PolyDate is forbidden")
+    def __lt__(self,other):
+        return self.universal()._n < other.universal()._n
+    def __gt__(self,other):
+        return self.universal()._n > other.universal()._n
+    def __le__(self,other):
+        return self.universal()._n <= other.universal()._n
+    def __ge__(self,other):
+        return self.universal()._n >= other.universal()._n
+    def __eq__(self,other):
+        return self.universal()._n == other.universal()._n
+    def __ne__(self,other):
+        return self.universal()._n != other.universal()._n
 
 # all child classes of PolyDate must have the following:
 
